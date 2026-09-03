1be60b8e09314c141603f40fd09afd86c8415cfb
DevEens-ali <eensali01@gmail.com>
2026-09-03 16:24:47 +0500

    Implement JWT authentication

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.database import engine, Base
 from app import models
 from app.routers import auth
-
+from app.routers import auth, users
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
@@ -12,6 +12,8 @@ app = FastAPI(
 )
 
 app.include_router(auth.router)
+app.include_router(auth.router)
+app.include_router(users.router)
 
 
 @app.get("/health")
