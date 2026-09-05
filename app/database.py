e7619009cf6c8f687130b33915d12f15046fa0f4
DevEens-ali <eensali01@gmail.com>
2026-09-05 19:41:13 +0500

    Debug database driver

--- app/database.py
+++ app/database.py
@@ -7,9 +7,18 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# load_dotenv(BASE_DIR / ".env", override=True)
+
+# DATABASE_URL = os.getenv("DATABASE_URL")
+
+# if DATABASE_URL is None:
+#     raise ValueError("DATABASE_URL not found in .env")
+
 load_dotenv(BASE_DIR / ".env", override=True)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("DATABASE DRIVER:", DATABASE_URL.split("://")[0] if DATABASE_URL else None)
 
 if DATABASE_URL is None:
     raise ValueError("DATABASE_URL not found in .env")
